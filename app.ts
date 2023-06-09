e1dad86c0964cc3880b6d3af6c21858b9864fc2e
Danimartinez25 <danimartinez1325@gmail.com>
2023-06-08 21:18:25 -0300

    create repository

---
+++ app.ts
@@ -0,0 +1,7 @@
+const integer: number = 6;
+const hexadecimal: number = 0xf00d;
+const binary: number = 0b1010;
+const octal: number = 0o744;
+const str : string = "FFFF";
+const bool = true;
+const daniel = 'true';
